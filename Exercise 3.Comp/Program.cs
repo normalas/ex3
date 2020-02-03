018233bf19d4bda2a494fba1832f9fdf49689a3f
Nikolai <n.svanaas@gmail.com>
2020-02-03 16:59:57 +0100

    -

---
+++ Exercise 3.Comp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.IO;
+using Humanizer.Bytes;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the full path to the file to compress: ");
+            string input = Console.ReadLine();
+            string fileToCompress = @""+ input;
+            long inFileSizeInBytes;
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Restart();
+
+            using (var inFile = File.OpenRead(fileToCompress))
+            {
+                inFileSizeInBytes = inFile.Length;
+
+                using (var outFile = File.Create(fileToCompress + ".gz"))
+                {
+
+                    using (var compressor = new GZipStream(outFile, CompressionMode.Compress))
+                    {
+                        inFile.CopyTo(compressor);
+                    }
+                }
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"Compressed a file of size {(new ByteSize(inFileSizeInBytes)).Megabytes:N} (MB) in {stopwatch.ElapsedMilliseconds / 1000} seconds.");
+        }
+    }
+}
---
+++ Exercise 3.DeComp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Humanizer.Bytes;
+using System;
+using System.Diagnostics;
+using System.IO.Compression;
+using static System.IO.File;
+
+namespace Exercise_3.DeComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the path to the file you wish to decompress: ");
+            string Input = Console.ReadLine();
+            var FileToDecompress = @"" + Input;
+            long inFileSizeInBytes;
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Restart();
+
+            using (var inFile = OpenRead(FileToDecompress))
+            {
+                inFileSizeInBytes = inFile.Length;
+
+                using (var outFile = Create(FileToDecompress))
+                {
+                    using (var compressor = new GZipStream(outFile, CompressionMode.Decompress))
+                    {
+                        inFile.CopyTo(compressor);
+                    }
+                }
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Decompressed a file of size {(new ByteSize(inFileSizeInBytes)).Megabytes:N} (MB) in {stopwatch.ElapsedMilliseconds/1000} seconds.");
+        }
+    }
+}
